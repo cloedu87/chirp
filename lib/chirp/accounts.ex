349cacf3b27fa9d6d04472fb0056bf07e37146ad
Claudio Siegenthaler <cloedu@gmail.com>
2025-06-15 18:54:36 +0200

    chore: housekeeping

--- lib/chirp/accounts.ex
+++ lib/chirp/accounts.ex
@@ -189,7 +189,7 @@ defmodule Chirp.Accounts do
   """
   def deliver_user_update_email_instructions(%User{} = user, current_email, update_email_url_fun)
       when is_function(update_email_url_fun, 1) do
-    {encoded_token, user_token} = UserToken.build_email_token(user, "change:#{current_email}")
+    {_encoded_token, user_token} = UserToken.build_email_token(user, "change:#{current_email}")
 
     Repo.insert!(user_token)
     # UserNotifier.deliver_update_email_instructions(user, update_email_url_fun.(encoded_token))
@@ -282,7 +282,7 @@ defmodule Chirp.Accounts do
     if user.confirmed_at do
       {:error, :already_confirmed}
     else
-      {encoded_token, user_token} = UserToken.build_email_token(user, "confirm")
+      {_encoded_token, user_token} = UserToken.build_email_token(user, "confirm")
       Repo.insert!(user_token)
       # UserNotifier.deliver_confirmation_instructions(user, confirmation_url_fun.(encoded_token))
     end
@@ -323,7 +323,7 @@ defmodule Chirp.Accounts do
   """
   def deliver_user_reset_password_instructions(%User{} = user, reset_password_url_fun)
       when is_function(reset_password_url_fun, 1) do
-    {encoded_token, user_token} = UserToken.build_email_token(user, "reset_password")
+    {_encoded_token, user_token} = UserToken.build_email_token(user, "reset_password")
     Repo.insert!(user_token)
 
     # UserNotifier.deliver_reset_password_instructions(user, reset_password_url_fun.(encoded_token))
